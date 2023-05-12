f9e8ae4873309d6349206ce5813fa04c946ab469
Yile Shen <yshen@eastsideprep.org>
2023-05-12 11:10:10 -0700

    Map Creation complete
    Completed the creation of the beatmap from a file

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,12 +1,15 @@
 extends Node3D
 
-var lane1X = -2.95
+var start = -19.5
 
-var lane2X = -0.9
+enum lanes {lane1, lane2, lane3, lane4}
 
-var lane3X = 1.05
-
-var lane4X = 3
+const lane_x = {
+	lanes.lane1 : -2.95,
+	lanes.lane2 : -0.9,
+	lanes.lane3 : 1.05,
+	lanes.lane4 : 3
+	}
 
 var lane1 = []
 var lane1_is_held = false
@@ -20,17 +23,58 @@ var lane3_is_held = false
 var lane4 = []
 var lane4_is_held = false
 
-var path = "res:/maps/"+"Awake Now"+"/"
-var song = "path" + "song.mp3"
-var map = "path" + "map.json"
+var path = "res://maps/"+Global.map+"/"
+var song = path + "song.mp3"
+var map = path + "map.json"
+
+var bpm = 117
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	load_song()
 	var music_player = AudioStreamPlayer.new()
 	music_player.stream = load(song)
 	add_child(music_player)
 	music_player.play()
-	load_song()
+	
+func load_lane(dict, lane):
+	var note = preload("res://assets/note.tscn")
+	var hold_start = preload("res://assets/hold_note_start.tscn")
+	var hold_inter = preload("res://assets/hold_note_intermediate.tscn")
+	var hold_end = preload("res://assets/hold_note_end.tscn")
+	var lane_notes = dict[lanes.keys()[lane]]
+	var note_start = start
+	for i in range(lane_notes.size()):
+		
+		if lane_notes[i] == 1:
+			var cur_note = note.instantiate()
+			add_child(cur_note)
+			var move_by = (((bpm/60.0)/16.0) * 19.5)
+			cur_note.position.x = lane_x[lane]
+			cur_note.position.z = start - move_by
+				
+		elif lane_notes[i] == 2:
+			var cur_note = hold_start.instantiate()
+			add_child(cur_note)
+			var move_by = (((bpm/60.0)/16.0) * 19.5)
+			cur_note.position.x = lane_x[lane]
+			cur_note.position.z = note_start - move_by
+		
+		elif lane_notes[i] == 3:
+			var cur_note = hold_inter.instantiate()
+			add_child(cur_note)
+			var move_by = (((bpm/60.0)/16.0) * 19.5)
+			cur_note.position.x = lane_x[lane]
+			cur_note.position.z = note_start - move_by
+		
+		elif lane_notes[i] == 4:
+			var cur_note = hold_end.instantiate()
+			add_child(cur_note)
+			var move_by = (((bpm/60.0)/16.0) * 19.5)
+			cur_note.position.x = lane_x[lane]
+			cur_note.position.z = note_start - move_by
+			
+		note_start -= (((bpm/60.0)/32.0) * 19.5)
 
 func load_song():
 	if not FileAccess.file_exists(map):
@@ -39,7 +83,10 @@ func load_song():
 		
 	var load_file = FileAccess.open(map, FileAccess.READ)
 	var dict = JSON.parse_string(load_file.get_as_text())
-	print(dict)
+	load_lane(dict, lanes.lane1)
+	load_lane(dict, lanes.lane2)
+	load_lane(dict, lanes.lane3)
+	load_lane(dict, lanes.lane4)
 	
 	
 func save():
@@ -81,13 +128,11 @@ func judge_lane(l):
 	var lane_pointer = lane.size()-1
 	if lane.size()-1 < 0:
 		lane_pointer = 0
-	
 	if !lane.is_empty():
 		if !is_instance_valid(lane[lane_pointer]):
 			lane.pop_back()
 			
 		if Input.is_action_just_pressed(laneHit):
-			print("hi")
 			print(lane[lane_pointer].get_judge()) #replace with showing judgement later
 			lane[lane_pointer].hit()
 			if lane[lane_pointer].get_type() == "hold_start":
@@ -121,6 +166,17 @@ func _physics_process(delta):
 	judge_lane(4)
 	
 
+func _on_lane_queue_1_body_entered(body):
+	lane1.push_front(body)
 
 
+func _on_lane_queue_2_body_entered(body):
+	lane2.push_front(body)
 
+
+func _on_lane_queue_3_body_entered(body):
+	lane3.push_front(body)
+
+
+func _on_lane_queue_4_body_entered(body):
+	lane4.push_front(body)
